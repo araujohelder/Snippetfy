a5d313db352a92577f59a8c7256f85b5665e64e8
araujohelder <heldersilvaaraujo@gmail.com>
2018-07-29 13:30:57 -0300

    layout e criacao de Snippet

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,4 +1,4 @@
-const { bcrypt } = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const { User } = require('../models');
 
 
@@ -7,17 +7,53 @@ module.exports = {
     return res.render('auth/signin');
   },
 
+  signout(req, res) {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  },
+
   signup(req, res) {
     return res.render('auth/signup');
   },
 
-  async register(req, res) {
-    const { email } = req.body;
-    if (await User.findOne({ where: { email } })) {
-      return res.redirect('back');
+  async register(req, res, next) {
+    try {
+      const { email } = req.body;
+      if (await User.findOne({ where: { email } })) {
+        req.flash('error', 'E-mail já cadastrado');
+        return res.redirect('back');
+      }
+      const password = await bcrypt.hash(req.body.password, 5);
+      await User.create({ ...req.body, password });
+      req.flash('success', 'Usuário cadastrado com sucesso');
+      return res.redirect('/');
+    } catch (err) {
+      return next(err);
     }
-    const password = await bcrypt.hash(req.body.password, 5);
-    await User.create({ ...req.body, password });
-    return res.redirect('/');
+  },
+
+  async authenticate(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        req.flash('error', 'Usuário inexistente');
+        return res.redirect('back');
+      }
+
+      if (!await bcrypt.compare(password, user.password)) {
+        req.flash('error', 'Senha incorreta');
+        return res.redirect('back');
+      }
+
+      req.session.user = user;
+
+      return req.session.save(() => {
+        res.redirect('app/dashboard');
+      });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
--- app/models/users.js
+++ app/models/users.js
@@ -4,5 +4,9 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
   });
+
+  User.associate = (models) => {
+    User.hasMany(models.Category);
+  };
   return User;
 };
